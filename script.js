086f2ab6332980d746a6564993b49f46c79146b5
Nalithdev <Ethan.delcroix@outlook.fr>
2023-01-11 11:26:50 +0100

    aléatoire

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let allpokemon = fetch('https://pokeapi.co/api/v2/pokemon/?limit=1008')
+
+let poke = []
+document.querySelector('.pokemon_area').innerHTML = '';
+for(let i =0 ; i<6 ; i++){
+    poke =Math.floor(Math.random()*  900 )
+    if(poke == 0){
+        poke =Math.floor(Math.random()*  900 )
+    }
+    else{
+
+console.log(poke)
+let allpokemon = fetch('https://pokeapi.co/api/v2/pokemon/'+ poke)
 allpokemon.then(function (response) {
     return response.json();
     }).then(function (data) {
     console.log(data);
-    document.querySelector('.pokemon_area').innerHTML = '';
-    for (let i = 0; i < data.results.length; i++) {
-        let li = document.createElement('li')
-        li.innerHTML = `<h2>${data.results[i].name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png">`
-        document.querySelector('.pokemon_area').appendChild(li)
-    }
+
+        let div = document.createElement('div')
+        div.innerHTML += `<div><h2>${data.name}</h2><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png"></div>`
+        document.querySelector('.pokemon_area').appendChild(div)
+
 });
+    }
+}
